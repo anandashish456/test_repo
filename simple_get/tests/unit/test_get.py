aa5bb9bcada479fcb7218753937d0d564adbe68e
Ashish Anand <anandashish456@gmail.com>
2019-06-04 15:10:45 +0530

    test

--- simple_get/tests/unit/test_get.py
+++ simple_get/tests/unit/test_get.py
@@ -6,11 +6,11 @@ import simple_get.app
 class TestTheGet(TestCase):
     def test_url(self):
         with patch('simple_get.app.requests.get') as mocked_call:
-            app.call_func()
+            simple_get.app.call_func()
             mocked_call.assert_called_with("https://www.google.com")
 
     def test_check_print(self):
         with patch('builtins.print') as mocked_print:
-            app.call_func()
+            simple_get.app.call_func()
             mocked_print.assert_called_with(200)
 
